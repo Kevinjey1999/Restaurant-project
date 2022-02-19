84d5b4503b4864bdba6abd99e765e1c56303dbce
Kevin james <kevinjey1999@gmail.com>
2022-02-19 16:56:07 +0330

    project completed

---
+++ resources/views/Admin/admin.blade.php
@@ -0,0 +1,16 @@
+@auth
+ <x-app-layout>
+
+ </x-app-layout>
+@endauth
+
+<!DOCTYPE html>
+<html lang="en">
+  @include('Admin.admincss')
+  <body>
+@include('Admin.navbar')
+
+
+@include('Admin.adminjs')
+  </body>
+</html>

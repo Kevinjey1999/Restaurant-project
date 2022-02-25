53b8f51033393a4cacb1587bda9b392c382a0278
hosein elahi <kevinjey1999@gmail.com>
2022-02-25 12:43:09 -0800

    Admin panel Completed

--- resources/views/Admin/admin.blade.php
+++ resources/views/Admin/admin.blade.php
@@ -1,16 +1,19 @@
-@auth
+{{-- @auth
  <x-app-layout>
 
  </x-app-layout>
-@endauth
+@endauth --}}
 
 <!DOCTYPE html>
 <html lang="en">
   @include('Admin.admincss')
   <body>
+    <div class="container-scroller">
 @include('Admin.navbar')
 
 @include('Admin.body')
+
+    </div>
 
 @include('Admin.adminjs')
   </body>
